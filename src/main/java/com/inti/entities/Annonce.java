b35befd510b9d965e488f2dd5aebd784d561155f
root <root@localhost.localdomain>
2022-06-07 16:26:27 +0200

    Ajout Classe Annonce

---
+++ src/main/java/com/inti/entities/Annonce.java
@@ -0,0 +1,40 @@
+package com.inti.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Annonce {
+	
+	private Long idAnnonce;
+	private Date dateAnnonce;
+	
+	public Annonce() {
+		
+	}
+
+	public Annonce(Date dateAnnonce) {
+		this.dateAnnonce = dateAnnonce;
+	}
+
+	public Long getIdAnnonce() {
+		return idAnnonce;
+	}
+
+	public void setIdAnnonce(Long idAnnonce) {
+		this.idAnnonce = idAnnonce;
+	}
+
+	public Date getDateAnnonce() {
+		return dateAnnonce;
+	}
+
+	public void setDateAnnonce(Date dateAnnonce) {
+		this.dateAnnonce = dateAnnonce;
+	}
+	
+	
+	
+	
+}
