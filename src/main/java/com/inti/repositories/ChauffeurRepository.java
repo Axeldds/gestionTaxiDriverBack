201511306e0fd85243813725ad4ad39ac665bb7d
BaptisteTINTI <taillefumierbaptsite@gmail.com>
2022-06-08 14:52:09 +0200

    Suppression methode findId

--- src/main/java/com/inti/repositories/ChauffeurRepository.java
+++ src/main/java/com/inti/repositories/ChauffeurRepository.java
@@ -6,11 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.inti.entities.Chauffeur;
+
 @Repository
-public interface ChauffeurRepository extends JpaRepository<Chauffeur, Long>{
-	
-	Chauffeur findByIdChauffeur(Long id);
-	
+public interface ChauffeurRepository extends JpaRepository<Chauffeur, Long> {
+
 	List<Chauffeur> findByNomChauffeur(String nomChauffeur);
+
 	List<Chauffeur> findByPrenomChauffeur(String prenomChauffeur);
 }
