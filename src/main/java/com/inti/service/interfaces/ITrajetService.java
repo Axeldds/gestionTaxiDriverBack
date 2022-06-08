574b8ba28b2358328ab852da1351c80492f15bc8
Axeldds <axel.didisse@gmail.com>
2022-06-08 11:38:41 +0200

    ITrajetService

---
+++ src/main/java/com/inti/service/interfaces/ITrajetService.java
@@ -0,0 +1,16 @@
+package com.inti.service.interfaces;
+
+import java.util.List;
+
+import com.inti.entities.Trajet;
+
+public interface ITrajetService {
+	
+	List<Trajet> findAll();
+
+	Trajet findOne(Long id);
+
+	Trajet save(Trajet trajet);
+
+	void delete(Long id);
+}
