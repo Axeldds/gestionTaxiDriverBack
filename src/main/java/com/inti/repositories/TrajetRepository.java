503f216995799387601d9941347fcac11af3cd83
Axeldds <axel.didisse@gmail.com>
2022-06-08 10:27:50 +0200

    TrajetRepository

---
+++ src/main/java/com/inti/repositories/TrajetRepository.java
@@ -0,0 +1,12 @@
+package com.inti.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.inti.entities.Trajet;
+
+@Repository
+public interface TrajetRepository extends JpaRepository<Trajet, Long>{
+	Trajet findByIdTrajet(Long idTrajet);
+	
+}
