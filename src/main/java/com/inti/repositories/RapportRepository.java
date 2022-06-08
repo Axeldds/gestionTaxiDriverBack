1fff0a54d8f78f29fceea379cc6b4b3e82ff5541
root <root@localhost.localdomain>
2022-06-08 10:21:36 +0200

    Rapport Repository

---
+++ src/main/java/com/inti/repositories/RapportRepository.java
@@ -0,0 +1,11 @@
+package com.inti.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.inti.entities.Rapport;
+
+@Repository
+public interface RapportRepository extends JpaRepository<Rapport,Long>{
+	Rapport findById(long id_rapport);
+}
