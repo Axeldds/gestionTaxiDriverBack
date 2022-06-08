78927894bf48512003098aa8a990b9bc924b9101
Axeldds <axel.didisse@gmail.com>
2022-06-08 10:32:46 +0200

    AvisRepository

--- src/main/java/com/inti/repositories/AvisRepository.java
+++ src/main/java/com/inti/repositories/AvisRepository.java
@@ -1,5 +1,12 @@
 package com.inti.repositories;
 
-public interface AvisRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.inti.entities.Avis;
+
+@Repository
+public interface AvisRepository extends JpaRepository<Avis, Long>{
+	
+	Avis findByIdAvis(Long idAvis);
 }
