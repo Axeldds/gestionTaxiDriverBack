7e572070bd972f2b68954130c13bcd895eca8230
root <root@localhost.localdomain>
2022-06-08 11:49:51 +0200

    IAgenceService, IResponsableAgenceService, IAnnonceService

---
+++ src/main/java/com/inti/service/interfaces/IResponsableAgenceService.java
@@ -0,0 +1,17 @@
+package com.inti.service.interfaces;
+
+import java.util.List;
+
+import com.inti.entities.ResponsableAgence;
+
+public interface IResponsableAgenceService {
+	List<ResponsableAgence> findAll();
+
+	ResponsableAgence findOne(Long id);
+
+	ResponsableAgence save(ResponsableAgence responsableAgence);
+
+	void delete(Long id);
+	
+	ResponsableAgence findByIdResp(Long idResponsable);
+}
